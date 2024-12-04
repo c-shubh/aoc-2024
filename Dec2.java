93c0ac8fc2ee73e28f47e76613b74079a943fa4c
Shubh A Chudasama <shubh.cs@proton.me>
2024-12-04 12:46:36 +0530

    Solve dec2 part2

--- Dec2.java
+++ Dec2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Dec2 {
 
-  static final boolean debug = true;
+  static final boolean debug = false;
 
   static class Part1 {
     static boolean isSafe(int[] nums) {
@@ -38,7 +38,70 @@ public class Dec2 {
     }
   }
 
+  static class Part2 {
+
+    static int longestIncreasingSubsequence(int[] a) {
+      int ans = Integer.MIN_VALUE;
+      // stores length of LIS ending at i
+      int[] dp = new int[a.length];
+      for (int i = 0; i < a.length; i++) {
+        dp[i] = 1; // itsef is a subseq
+        // check all prev LIS
+        for (int j = i - 1; j >= 0; j--) {
+          int diff = a[i] - a[j];
+          if (1 <= diff && diff <= 3) {
+            dp[i] = Math.max(dp[i], 1 + dp[j]);
+          }
+        }
+        ans = Math.max(ans, dp[i]);
+      }
+      return ans;
+    }
+
+    static int longestDecreasingSubsequence(int[] a) {
+      int ans = Integer.MIN_VALUE;
+      // stores length of LDS ending at i
+      int[] dp = new int[a.length];
+      for (int i = 0; i < a.length; i++) {
+        dp[i] = 1; // itself is a subseq
+        // check all prev LDS
+        for (int j = i - 1; j >= 0; j--) {
+          int diff = a[j] - a[i]; // reversed subtraction for decreasing
+          if (1 <= diff && diff <= 3) {
+            dp[i] = Math.max(dp[i], 1 + dp[j]);
+          }
+        }
+        ans = Math.max(ans, dp[i]);
+      }
+      return ans;
+    }
+
+    static boolean isSafe(int[] nums) {
+      return longestIncreasingSubsequence(nums) >= nums.length - 1
+          || longestDecreasingSubsequence(nums) >= nums.length - 1;
+    }
+
+    public static void main() {
+      Scanner sc = new Scanner(System.in);
+      int safeCount = 0;
+      while (sc.hasNextLine()) {
+        int[] nums = Arrays.stream(
+            sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        if (isSafe(nums)) {
+          safeCount++;
+          if (debug)
+            System.out.println("Safe: " + Arrays.toString(nums));
+        } else {
+          if (debug)
+            System.out.println("Unsafe: " + Arrays.toString(nums));
+        }
+      }
+      System.out.println(safeCount);
+      sc.close();
+    }
+  }
+
   public static void main(String[] args) {
-    Part1.main();
+    Part2.main();
   }
 }
